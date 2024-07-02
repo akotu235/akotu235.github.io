98e0a44eb5588acc17f85042d4d77045c8e760e9
akotu235 <akotu@protonmail.com>
2024-07-02 09:21:15 +0200

    Blocked scaling

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -321,3 +321,18 @@ document.addEventListener("DOMContentLoaded", function () {
         overlay.style.opacity = '0';
     });
 });
+
+document.addEventListener('touchstart', function(event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}, { passive: false });
+
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function(event) {
+    let now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd = now;
+}, false);
